80da521d74896cad9fe70b28153667b2d2f11c9e
BOMB-IRON22 <110793614+BOMB-jpg@users.noreply.github.com>
2024-02-16 15:13:22 +0800

    Update StockMarketDataAnalysis.cs

--- StockMarketComponent/StockMarketDataAnalysis.cs
+++ StockMarketComponent/StockMarketDataAnalysis.cs
@@ -12,7 +12,7 @@ namespace StockMarketComponent
         { 
             //Code goes here to set a member variable to the passed in data
         }
-
+    // 计算快速变化平均值
         public string CalculateFastMovingAverage()
         {
             Thread.Sleep(6000);
@@ -20,6 +20,7 @@ namespace StockMarketComponent
             return $"{nameof(CalculateFastMovingAverage)} - ThreadId: {Thread.CurrentThread.ManagedThreadId}";
 
         }
+        //方法模拟计算慢速移动平均值的操作
         public string CalculateSlowMovingAverage()
         {
             Thread.Sleep(7000);
@@ -27,13 +28,16 @@ namespace StockMarketComponent
             return $"{nameof(CalculateSlowMovingAverage)} - ThreadId: {Thread.CurrentThread.ManagedThreadId}";
 
         }
+        //法模拟计算股票随机指标的操作，同
         public string CalculateStockastics()
         {
                 Thread.Sleep(10000);
+            //nameof（) 获取成员变量的名字
 
                 return $"{nameof(CalculateStockastics)} - ThreadId: {Thread.CurrentThread.ManagedThreadId}";
  
         }
+        //方法模拟计算布林带指标的操作，
         public string CalculateBollingerbands()
         {
             Thread.Sleep(5000);
